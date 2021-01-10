2b9cab7e1266efdea11eb26050b806db4c020dbc
Duli97 <dwindula@gmail.com>
2021-01-11 01:11:19 +0530

    integrating template

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { ProdcutService } from 'src/app/services/prodcut.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
