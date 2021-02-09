6ea8cbf85e43bcff4c3cf2c075ee8f6b95f22bd1
Duli97 <dwindula@gmail.com>
2021-02-09 23:44:02 +0530

    up to validation

---
+++ src/app/validators/the-D-shop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { TheDShopValidators } from './the-D-shop-validators';
+
+describe('Luv2ShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new TheDShopValidators()).toBeTruthy();
+  });
+});
